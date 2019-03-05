6518cebd1a52e620a9a293eb5a7ac02d78d64b0c
xolaniloni <xolaniloni@gmail.com>
2019-03-05 16:27:44 +0200

    added five statements

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -4,8 +4,11 @@ namespace ConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {
-           
+        { 
+            System.Console.WriteLine();
+        System.Console.WriteLine();
+        System.Console.WriteLine();
+        System.Console.WriteLine();
         }
     }
 }
