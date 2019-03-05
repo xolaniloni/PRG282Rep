f87cf31cb1e88e4d27ddc79daa26369626fc6438
xolaniloni <xolaniloni@gmail.com>
2019-03-05 16:36:57 +0200

    my first statement

--- ConsoleAp/ConsoleAp/Properties/AssemblyInfo.cs
+++ ConsoleAp/ConsoleAp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp")]
+[assembly: AssemblyTitle("ConsoleAp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp")]
+[assembly: AssemblyProduct("ConsoleAp")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4375be6c-a724-47eb-b86e-f9e66013743e")]
+[assembly: Guid("9b2062ea-47e3-47ca-b5e8-68fc590618f6")]
 
 // Version information for an assembly consists of the following four values:
 //
