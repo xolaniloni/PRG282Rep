1aa13a06abf699ea0ecd347b72ceb77ad6c1cdf4
xolaniloni <xolaniloni@gmail.com>
2019-03-05 15:16:34 +0200

    deleted

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
-            Console.WriteLine();
+        
         }
     }
 }
