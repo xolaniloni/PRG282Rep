6e7635785e094bbb0b1ff698d24357c1217c56e6
xolaniloni <xolaniloni@gmail.com>
2019-03-05 15:21:43 +0200

    two statements added

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+        }
+    }
+}
--- ConsoleApp/ConsoleApp/Properties/AssemblyInfo.cs
+++ ConsoleApp/ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("539db17a-6c68-4a5a-9524-74835a532228")]
+[assembly: Guid("4375be6c-a724-47eb-b86e-f9e66013743e")]
 
 // Version information for an assembly consists of the following four values:
 //
